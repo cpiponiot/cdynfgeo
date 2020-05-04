eb3d73ecc76cd480c0355626b2080825a08ff444
Camille Piponiot <camille.piponiot@gmail.com>
2020-05-04 10:20:35 -0500

    finished prepare_data() function

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -35,6 +35,7 @@ prepare_data <- function(path,
                                           "agb", "brokenladder", "fire.code") # for Yosemite
 )
 {
+  library(data.table)
   print(paste("Preparing", site, "data."))
   data("site_info")
   # load packages (and install the ones are not yet installed)
@@ -106,7 +107,7 @@ prepare_data <- function(path,
   DT$exactdate = as.character(DT$exactdate) ## convert to always have characters
 
   # calculate census year
-  census_year = DT[, .(year = get_census_year(exactdate)), by = census]
+  census_year = DT[, list(year = get_census_year(exactdate)), by = census]
   DT = merge(DT, census_year)
 
   # empty DFstatus
@@ -145,3 +146,63 @@ prepare_data <- function(path,
 
   return(DT)
 }
+
+#' Extract census year from a set of exact measure dates
+#' @param date vector of exact measurement dates, containing the year,
+#'             month and day (not always in this order) and sometimes
+#'             the hour of all measurements during one census
+#' @return A numeric vector with census years
+#' @export
+#'
+get_census_year = function(date) {
+
+  # remove hours
+  split_date = data.table::tstrsplit(date, " ")[[1]]
+
+  # separate year, month and day
+  split_date = data.table::tstrsplit(split_date, "-|/")
+
+  # the one in the middle cannot be the year
+  split_date = list(split_date[[1]], split_date[[3]])
+
+  split_date = lapply(split_date, as.numeric)
+
+  ## remove empty dates (zeros)
+  zeros = which(split_date[[1]]==0 & split_date[[2]]==0)
+  if (length(zeros) > 0)
+    split_date = lapply(split_date, function(x) x[-zeros])
+
+  # because they are not always in the same order (depending on the site),
+  # and the year is in different formats (2 or 4 digits)
+  # we chose the value > 31 when there is one,
+  # or the value with the smallest range across tree measurements
+
+  if (any(split_date[[1]] > 31, na.rm = TRUE)) {
+    year = split_date[[1]]
+  } else if (any(split_date[[2]] > 31, na.rm = TRUE)) {
+    year = split_date[[2]]
+  } else {
+    range1 = diff(range(split_date[[1]], na.rm = TRUE))
+    range2 = diff(range(split_date[[2]], na.rm = TRUE))
+
+    if ((range1 > 2 & range2 > 2) | range1 == range2)
+      stop("There is a problem with the date format.")
+
+    year = split_date[[which.min(c(range1, range2))]]
+  }
+
+  ## if census year has less than 4 digits: add 19- or 20-
+  if (all(year < 100, na.rm = TRUE))   {
+    add_1900 = which(!is.na(year) & year >= 50)
+    add_2000 = which(!is.na(year) & year < 50)
+    year[add_1900] = 1900 + year[add_1900]
+    year[add_2000] = 2000 + year[add_2000]
+  }
+
+  census_year = round(mean(year, na.rm = TRUE))
+
+
+  return(census_year)
+
+}
+
