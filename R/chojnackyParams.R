e6ecc955fb3f4ee29fa1f45c4f20bbbe61239920
Camille Piponiot <camille.piponiot@gmail.com>
2020-05-06 19:10:26 -0500

    added a few equations to chojnackyParams

--- R/chojnackyParams.R
+++ R/chojnackyParams.R
@@ -13,33 +13,32 @@ chojnackyParams = function(family, genus, wsg) {
 
   pars[family == "Cupressaceae" & wsg < 0.3, 1] = -1.9615
   pars[family == "Cupressaceae" & wsg < 0.3, 2] = 2.1063
-
   pars[family == "Cupressaceae" & wsg >= 0.3 & wsg < 0.4, 1] = -2.7765
   pars[family == "Cupressaceae" & wsg >= 0.3 & wsg < 0.4, 2] = 2.4195
-
   pars[family == "Cupressaceae" & wsg >= 0.4, 1] = -2.6327
   pars[family == "Cupressaceae" & wsg >= 0.4, 2] = 2.4757
 
   pars[family == "Betulaceae" & wsg < 0.4, 1] = -2.5932
   pars[family == "Betulaceae" & wsg < 0.4, 2] = 2.5349
-
   pars[family == "Betulaceae" & wsg >= 0.4 & wsg < 0.5, 1] = -2.2271
   pars[family == "Betulaceae" & wsg >= 0.4 & wsg < 0.5, 2] = 2.4513
-
   pars[family == "Betulaceae" & wsg >= 0.5 & wsg < 0.6, 1] = -2.7765
   pars[family == "Betulaceae" & wsg >= 0.5 & wsg < 0.6, 2] = 2.348
-
   pars[family == "Betulaceae" & wsg >= 0.6, 1] = -2.2652
   pars[family == "Betulaceae" & wsg >= 0.6, 2] = 2.5349
 
   pars[family == "Pinaceae" & wsg < 0.45, 1] = -2.6177
   pars[family == "Pinaceae" & wsg < 0.45, 2] = 2.4638
-
   pars[family == "Pinaceae" & wsg >= 0.45, 1] = -3.0506
   pars[family == "Pinaceae" & wsg >= 0.45, 2] = 2.6465
 
   pars[family == "Fagaceae", 1] = -2.0705
   pars[family == "Fagaceae", 2] = 2.4410
+
+  pars[family == "Salicaceae" & wsg < 0.35, 1] = -2.6863
+  pars[family == "Salicaceae" & wsg < 0.35, 2] = 2.4561
+  pars[family == "Salicaceae" & wsg >= 0.35, 1] = -2.4441
+  pars[family == "Salicaceae" & wsg >= 0.35, 2] = 2.4561
 
   pars[family == "Hamamelidaceae", 1] = -2.6390
   pars[family == "Hamamelidaceae", 2] = 2.5466
@@ -52,9 +51,13 @@ chojnackyParams = function(family, genus, wsg) {
 
   pars[family == "Oleaceae" & wsg < 0.55, 1] = -2.0314
   pars[family == "Oleaceae" & wsg < 0.55, 2] = 2.3524
-
   pars[family == "Oleaceae" & wsg >= 0.55, 1] = -1.8384
   pars[family == "Oleaceae" & wsg >= 0.55, 2] = 2.3524
+
+  pars[family == "Aceraceae" & wsg < 0.5, 1] = -2.0470
+  pars[family == "Aceraceae" & wsg < 0.5, 2] = 2.3852
+  pars[family == "Aceraceae" & wsg >= 0.5, 1] = -1.8011
+  pars[family == "Aceraceae" & wsg >= 0.5, 2] = 2.3852
 
   pars[genus == "Abies" & wsg < 0.35, 1] = -2.3123
   pars[genus == "Abies" & wsg < 0.35, 2] = 2.3482
